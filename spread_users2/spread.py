ed4894e00f1ce8d7eb68ccc08ec6934b7e4d1a1f
unknown <mohammadkhahamir@gmail.com>
2024-02-14 01:37:15 +0330

    for user in mikrotik convrt fromat user and pass

---
+++ spread_users2/spread.py
@@ -0,0 +1,27 @@
+'''I have created new users in mikrotik and I want to send username and password in telegram in specific format so I wrote this script.'''
+
+import re
+try:
+    file = open("users2.txt" , 'r')
+    text = file.read()
+    result = re.findall('name=.*password=.....'  ,  text)
+    file.close()
+except:
+    print("error")
+users = {}
+for item in result:
+    user , password = item.split(' ')
+    user = user.split('=')[1]
+    password = password.split('=')[1]
+    users[user] = password
+try:
+    file = open('users2-final.txt' , 'w+')
+    for user , passw in users.items():
+        file.write(f"user: {user}\npass: {passw}\n\n")
+    file.close()
+except:
+    print("error")
+
+
+
+print(users)
